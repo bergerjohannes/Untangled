1bdd600c917740eaabc1b7794561abc935902b27
Johannes Berger <johannes@berger.ac>
2023-12-08 14:14:49 +0100

    Update navigation bar

--- app/components/navigationBar.tsx
+++ app/components/navigationBar.tsx
@@ -3,8 +3,6 @@ import { SimpleLogo } from './logo'
 import { useOutletContext } from '@remix-run/react'
 import { SupabaseOutletContext } from '~/root'
 import { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import { Session, User } from '@supabase/gotrue-js/src/lib/types'
 
@@ -12,13 +10,31 @@ const NavigationBar = ({ withoutMenu = false }) => {
   const { supabase } = useOutletContext<SupabaseOutletContext>()
   const [user, setUser] = useState<User | null>(null)
   const [session, setSession] = useState<Session | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data }) => {
       setSession(data.session)
       setUser(data.session?.user ?? null)
+      setLoading(false)
     })
   }, [])
+
+  const renderLogo = () => (
+    <li>
+      <Link to='/' className='flex items-center justify-center mr-4'>
+        <SimpleLogo />
+      </Link>
+    </li>
+  )
+
+  if (loading) {
+    return (
+      <div className='w-full p-4' style={{ backdropFilter: 'blur(10px)' }}>
+        <ul className='flex justify-start items-center'>{renderLogo()}</ul>
+      </div>
+    )
+  }
 
   return (
     <nav
@@ -28,33 +44,29 @@ const NavigationBar = ({ withoutMenu = false }) => {
       }}
     >
       <div className='w-full p-4' style={{ backdropFilter: 'blur(10px)' }}>
-        <ul className='flex justify-between'>
-          <li>
-            <Link to='/' className='flex items-center justify-center'>
-              <SimpleLogo />
-            </Link>
-          </li>
-          {!withoutMenu && (
-            <li className='relative group'>
-              <ul className='absolute -right-1 -top-1 mt-2 p-2 bg-blackish text-whitish rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 border-2 border-whitish'>
-                <li>
-                  <Link
-                    to='/profile'
-                    className='block px-2 py-1 rounded hover:bg-whitish hover:text-blackish'
-                  >
-                    Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to='/notes'
-                    className='block px-2 py-1 rounded hover:bg-whitish hover:text-blackish'
-                  >
-                    Notes
-                  </Link>
-                </li>
-              </ul>
-              <FontAwesomeIcon icon={faBars} className='cursor-pointer' />
+        <ul className='flex justify-start items-center'>
+          {renderLogo()}
+          {!user && (
+            <li className='flex space-x-4'>
+              <Link to='/' className='px-2 rounded hover:opacity-100 opacity-40'>
+                Record
+              </Link>
+              <Link to='/signup' className='px-2 rounded hover:opacity-100 opacity-40'>
+                Sign Up
+              </Link>
+            </li>
+          )}
+          {user && !withoutMenu && (
+            <li className='flex space-x-4'>
+              <Link to='/' className='px-2 rounded hover:opacity-100 opacity-40'>
+                Record
+              </Link>
+              <Link to='/notes' className='px-2 rounded hover:opacity-100 opacity-40'>
+                Notes
+              </Link>
+              <Link to='/profile' className='px-2 rounded hover:opacity-100 opacity-40'>
+                Profile
+              </Link>
             </li>
           )}
         </ul>
